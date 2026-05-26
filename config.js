a3fec88e8bd1e2694b69e9a60e655fa2941b4226
louistrankhanhhung-arch <louis.trankhanhhung@gmail.com>
2026-05-27 02:07:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.ELT_CONFIG = {
   API_BASE: "https://elt-production.up.railway.app/api",
   BASE_URL: "https://rootenglishhouse.com/",
-  MEDIA_BASE: "https://media.rootenglishhouse.com"
+  MEDIA_BASE: "https://media.rootenglishhouse.com",
   TEACHER_PIN: "0612"
 };
