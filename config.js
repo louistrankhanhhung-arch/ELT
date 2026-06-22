312a6559b954eb0709e741552e42aca060843096
louistrankhanhhung-arch <louis.trankhanhhung@gmail.com>
2026-06-22 20:38:10 +0700

    Update TEACHER_PIN in config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.ELT_CONFIG = {
   API_BASE: "https://elt-production.up.railway.app/api",
   BASE_URL: "https://rootenglishhouse.com/",
   MEDIA_BASE: "https://media.rootenglishhouse.com",
-  TEACHER_PIN: "0382"
+  TEACHER_PIN: "0612"
 };
